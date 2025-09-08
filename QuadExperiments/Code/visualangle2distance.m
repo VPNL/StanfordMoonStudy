54ba51c0e4dfb99b0a8c19ec65bf96279df61dac
peterxux <138543446+peterxux@users.noreply.github.com>
2025-09-08 16:23:21 -0700

    QuadExperiments

---
+++ QuadExperiments/Code/visualangle2distance.m
@@ -0,0 +1,8 @@
+function [distance] = visualangle2distance(visual_angle,height)
+% 
+% [distance] = visualangle2distance(visual_angle,height)
+% calculates the distance of an object of certain height that extends a certain visual angle
+% distace is the same units as height
+% visual angle is in degrees
+    distance =height/(2*tan(pi*visual_angle/180));
+end
